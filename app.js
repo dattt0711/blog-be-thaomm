500221e9bd3af6fe53219b69d69b83a15741805a
dattt0711 <1801040060@s.hanu.edu.vn>
2023-05-15 19:02:11 +0700

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+mongoose.connect('mongodb://127.0.0.1:27017/blog');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+    console.log('database connected');
+})
+
+
+const commentRoute = require('./routes/commentRoute')
+const blogRoute = require('./routes/blogRoute')
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use('/', commentRoute);
+app.use('/', blogRoute);
+app.listen(3003, () => {
+    console.log('listen on port 3003')
+})
